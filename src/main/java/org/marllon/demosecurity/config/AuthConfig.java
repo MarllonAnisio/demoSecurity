f37498e2268a96334bd11a35a619e137672e2277
MarllonAnisio <marllon.anisio@hotmail.com>
2026-03-30 19:00:07 -0300

    feat: add AuthConfig service for user authentication and registration

---
+++ src/main/java/org/marllon/demosecurity/config/AuthConfig.java
@@ -0,0 +1,35 @@
+package org.marllon.demosecurity.config;
+
+import org.marllon.demosecurity.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthConfig implements UserDetailsService {
+
+     private final UserRepository userRepository;
+
+
+     public AuthConfig(UserRepository userRepository) {
+         this.userRepository = userRepository;
+     }
+
+    /**
+     * Locates the user based on the username. In the actual implementation, the search
+     * may possibly be case sensitive, or case insensitive depending on how the
+     * implementation instance is configured. In this case, the <code>UserDetails</code>
+     * object that comes back may have a username that is of a different case than what
+     * was actually requested..
+     *
+     * @param username the username identifying the user whose data is required.
+     * @return a fully populated user record (never <code>null</code>)
+     * @throws UsernameNotFoundException if the user could not be found or the user has no
+     *                                   GrantedAuthority
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
