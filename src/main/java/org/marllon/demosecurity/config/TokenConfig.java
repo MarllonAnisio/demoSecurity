44f776de660a3e41365c3d6c3e1c46bccc0fae93
MarllonAnisio <marllon.anisio@hotmail.com>
2026-03-30 19:01:14 -0300

    feat: add TokenConfig for JWT token generation and validation

---
+++ src/main/java/org/marllon/demosecurity/config/TokenConfig.java
@@ -0,0 +1,46 @@
+package org.marllon.demosecurity.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.marllon.demosecurity.entity.User;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.Optional;
+
+@Component
+public class TokenConfig {
+    private String secret = "secret";
+
+    public String generateToken(User user) {
+
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        return JWT.create()
+                .withClaim("userId", user.getId())
+                .withSubject(user.getEmail())
+                .withExpiresAt(Instant.now().plusSeconds(86400))
+                .withIssuedAt(Instant.now())
+                .sign(algorithm);
+
+    }
+    public Optional<JWTUserData> validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            DecodedJWT decode = JWT.require(algorithm)
+                    .build()
+                    .verify(token);
+
+            return Optional.of(JWTUserData.builder()
+                    .userId(decode.getClaim("userId").asLong())
+                    .email(decode.getSubject())
+                    .build());
+
+        }catch (JWTVerificationException e){
+           return Optional.empty();
+        }
+    }
+}
